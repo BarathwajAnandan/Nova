9cd06c7af646500bb4c06406c8a81a8811d774fb
grohith327 <grohith327@gmail.com>
2025-10-18 09:07:14 -0700

    Initial Commit

---
+++ nova/novaApp.swift
@@ -0,0 +1,17 @@
+//
+//  novaApp.swift
+//  nova
+//
+//  Created by Rohith Gandhi  on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct novaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
